02d58873754c022a5657bdc132fb29c8d3e9c565
Caio César Beleza <caio.almeidabeleza@gmail.com>
2023-07-18 13:21:25 -0300

    Calculando Saldo Total

--- react-docker/src/pages/Home.js
+++ react-docker/src/pages/Home.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect} from "react";
 import axios from "axios";
-import Table from 'react-bootstrap/Table';
 import * as ReactBootstrap from 'react-bootstrap';
 
 export default function Home() {
@@ -8,10 +7,18 @@ export default function Home() {
   const [startDate, setStartDate] = useState('');
   const [endDate, setEndDate] = useState('');
   const [nomeOperador, setNome] = useState('');
+  const [saldoTotal, setSaldoTotal] = useState('');
 
   useEffect(() => {
     getTransferencias();
   }, []);
+
+  useEffect(() => {
+    getSaldoTotal(transferencias);
+  }, [transferencias]);
+
+
+
 
   const getTransferencias = async () => {
     try {
@@ -22,49 +29,84 @@ export default function Home() {
     }
   };
 
+  const getSaldoTotal = async (transferencias) => {
+    let soma = transferencias.reduce((a, b) => a + b.valor, 0);
+    setSaldoTotal(soma);
+  }
+
+
+  const getTransferenciasFiltros = async () => {
+    try {
+      const response = await axios.get("http://localhost:8080/procurar-filtros",
+      {
+        params: {
+            inicio: startDate,
+            fim: endDate,
+            nomeOperador: nomeOperador
+        }
+      });
+      setTransferencias(response.data);
+    } catch (error) {
+      console.error('Erro ao obter dados da API:', error);
+    }
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    getTransferencias();
+    getTransferenciasFiltros();
   };
 
   return (
     <div className="app-container">
       <div className="filters-container">
-        <h2>Informe os filtros:</h2>
+        <h2>Visualizar Transações:</h2>
         <form onSubmit={handleSubmit} className="filter-form">
-          <div className="form-group">
+            <div className="form-row">
+          <div className="col">
             <label>Data inicial:</label>
             <input
-              type="text"
+              type="date"
               className="form-control"
               value={startDate}
               onChange={(e) => setStartDate(e.target.value)}
+              placeholder="Data Inicial"
             />
           </div>
-          <div className="form-group">
+          <div className="col">
             <label>Data final:</label>
             <input
-              type="text"
+              type="date"
               className="form-control"
               value={endDate}
               onChange={(e) => setEndDate(e.target.value)}
+              placeholder="Data Final"
             />
           </div>
-          <div className="form-group">
-            <label>Texto:</label>
+          <div className="col">
+            <label>Nome:</label>
             <input
               type="text"
               className="form-control"
               value={nomeOperador}
               onChange={(e) => setNome(e.target.value)}
+              placeholder="Nome do Operador"
             />
+          </div>          
+          <button type="submit" className="btn btn-outline-dark btn-lg">Buscar</button>
           </div>
-          <button type="submit" className="btn btn-primary">Filtrar</button>
         </form>
       </div>
+      <ReactBootstrap.Container className='justify-content-left align-items-left'>
+      <div className="card text-white bg-dark mb-3">
+        <div className="card-body">
+            <h5 className="card-title">Saldo</h5>
+            <p className="card-text">Saldo Total: R$ {saldoTotal}</p>
+            <p className="card-text">Saldo no Período: R$</p>            
+        </div>
+      </div>
+      </ReactBootstrap.Container>
 
       <div className="table-container">
-        <h2>Tabela de Informações</h2>
         <ReactBootstrap.Table striped bordered hover variant="dark">
           <thead>
             <tr>
